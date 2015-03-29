bf61f13ea619ea02dda258d7ccd259ea33b8c46e
npoles <nickpoles@gmail.com>
2015-03-29 16:07:08 -0400

    Changed permissions on in-place ipbeacon.conf and ipbeacon.py to prevent unnecessary access.

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,13 +2,22 @@
 
 #****************************************************************************
 # install.sh
-# 
-# FILL THIS OUT! 
-# [DESCRIPTION]
 #
 # Author: npoles
 # Date: March 25, 2015
-# Last edited: March 25, 2015
+# Last edited: March 29, 2015
+#
+# This script moves all necessary ipbeacon files into place. These files 
+# include ipbeacon.py, ipbeacon.conf, and com.nickpoles.ipbeacon.plist. If
+# a necessary directory is not present, it will be created. A log file is also
+# created at /var/log/ipbeacon.log in case debugging is necessary. 
+#
+# ipbeacon.py (and ipbeacon.conf) is placed at /usr/local/libexec per Apple's
+# "Creating Launch Daemons and Agents" documentation which can be found here:
+# https://developer.apple.com/library/mac/documentation/MacOSX/Conceptual/BPSystemStartup/Chapters/CreatingLaunchdJobs.html#//apple_ref/doc/uid/10000172i-SW7-BCIEDDBJ 
+#
+# I'm more than open to suggestions about config file placement as libexec
+# doesn't feel like the correct place for that file to live. 
 # 
 # Preconditions:
 #   1. ipbeacon directory is in place and contains original files
@@ -33,8 +42,8 @@ sudo cp ../scripts/ipbeacon.py /usr/local/libexec/
 
 echo "Changing permissions..."
 sudo chmod 755 /usr/local/libexec
-sudo chmod 744 /usr/local/libexec/ipbeacon.py
-sudo chmod 644 /usr/local/libexec/ipbeacon.conf
+sudo chmod 700 /usr/local/libexec/ipbeacon.py
+sudo chmod 400 /usr/local/libexec/ipbeacon.conf
 
 
 echo "Creating log file at /var/log/ipbeacon.log..."
